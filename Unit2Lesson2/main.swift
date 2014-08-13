5d2092f8f1d67c219b033ae77b0e01b51a29de13
Eric Tuchscherer <eric@tuchscherer.me>
2014-08-13 11:19:10 -0700

    Initial Commit

---
+++ Unit2Lesson2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Unit2Lesson2
+//
+//  Created by Eric Tuchscherer on 8/13/14.
+//  Copyright (c) 2014 Nobody Special. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
