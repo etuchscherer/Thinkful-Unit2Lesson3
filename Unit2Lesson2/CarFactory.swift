89ea94c9266e0eac7294787f2820d1bfea9c8fda
Eric Tuchscherer <eric@tuchscherer.me>
2014-08-13 11:33:58 -0700

    Unit2Lesson - completed.

---
+++ Unit2Lesson2/CarFactory.swift
@@ -0,0 +1,16 @@
+//
+//  CarFactory.swift
+//  Unit2Lesson2
+//
+//  Created by Eric Tuchscherer on 8/13/14.
+//  Copyright (c) 2014 Nobody Special. All rights reserved.
+//
+
+import Foundation
+
+class CarFactory {
+    var name = ""
+    var color = ""
+    var horsepower = 0
+    var automaticOption = false
+}
